ffa90f3bf775271e068f50e385ba6101fdf55615
lyh <1243817329@qq.com>
2017-04-19 00:08:17 +0800

    --vuex的基础练习

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import './css/index.css';
 import Home from './components/Home'
 import  router from './routers';
+import  store from './store';
 new Vue({
     el: '#app',
     router,
+    store,
     render: v => v(Home)
 });
--- src/routers/Foo.vue
+++ src/routers/Foo.vue
@@ -3,11 +3,48 @@
         <h2>hello world!</h2>
         <p>
             {{name}}
+            {{$store.state.loginData.isLogin}}<br>
+            {{$store.state.count}}
         </p>
+        <form class="login-from" >
+            <div class="form-group">
+              <span class="form-name"> 用户名：</span> <input type="text">
+            </div>
+            <div class="form-group">
+                <span class="form-name">密码：</span> <input type="password">
+            </div>
+            <div class="form-group">
+                <button type="button" @click="login">登录</button>
+            </div>
+        </form>
     </div>
 </template>
+<style>
+    .login-from{
+        width: 500px;
+        margin: 0 auto;
+        padding: 25px;
+    }
+    .form-group{
+        margin-bottom: 25px;
+    }
+</style>
 <script>
+    import { mapGetters ,mapActions} from 'vuex'
+
     export  default{
-        name: "foo"
+        name: "foo",
+        data () {
+           return{
+               name:"foo"
+           }
+        },
+        computed: mapGetters([
+            'count',
+            'recentHistory'
+        ]),
+        methods: mapActions([
+            'login'
+        ])
     }
 </script>
