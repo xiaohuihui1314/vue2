302623f0e8cc68dbdc8fe176cd29e3720f6a918d
lyh <1243817329@qq.com>
2017-04-19 19:01:45 +0800

    --vuex2

--- src/routers/Foo.vue
+++ src/routers/Foo.vue
@@ -31,7 +31,11 @@
 </style>
 <script>
     import {mapActions} from 'vuex'
-
+    //后续在组件中使用的过程中，如果想要获取对应的状态你就可以直接使用this.$store.state获取，
+    // 当一个组件需要获取多个状态时候，将这些状态都声明为计算属性会有些重复和冗余。为了解决这个问题，我们可以使用 mapState 辅助函数帮助我们生成计算属性，让你少按几次键：
+    //mapGetters工具函数会将store中的getter映射到局部计算属性中。
+    //mapActions与mapMutations类似，也是绑定在组件的methods上的,如果选择直接触发的话，使用this.$store.dispatch(actionName)方法。
+    //mapMutations直接将触发函数映射到methods上，这样就能在元素事件绑定上直接使用了
     export  default{
         name: "foo",
         data () {
